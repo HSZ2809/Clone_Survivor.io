509c52304de550a3dd04077be23bcdd356b153a7
ZUN <94329618+HSZ2809@users.noreply.github.com>
2025-03-13 13:36:13 +0900

    Update Btn_SelectSkill.cs
    star 추가
    Blink 효과 추가

--- Assets/Scripts/Chapter/Button/Btn_SelectSkill.cs
+++ Assets/Scripts/Chapter/Button/Btn_SelectSkill.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening
 
 namespace ZUN
 {
@@ -11,6 +13,7 @@ namespace ZUN
         [SerializeField] Sprite[] sprite;
         [SerializeField] Image panelImage;
         [SerializeField] TextMeshProUGUI newTxt;
+        [SerializeField] Image[] stars;
 
         [Space]
         public Skill skill;
@@ -27,6 +30,14 @@ namespace ZUN
             skillName.text = skill.SkillName;
             skillImage.sprite = skill.Sprite;
             upgradeInfo.text = skill.UpgradeInfos[skill.Level];
+            int index = 0;
+            while (index < skill.Level)
+            {
+                // stars[i]. 알파를 1로 변경
+                stars[i].gameObject.SetActive(true);
+                index += 1;
+            }
+            StartCoroutine(BlinkStar(stars[index]));
 
             panelImage.sprite = sprite[(int)skillType];
             if(skill.Level < 1)
@@ -37,6 +48,15 @@ namespace ZUN
         {
             if (newTxt.gameObject.activeSelf)
                 newTxt.gameObject.SetActive(false);
+        }
+
+        IEnumerator BlinkStar(Image star)
+        {
+            star.DOFade(1.0f, 1.0f).SetLoops(2, LoopType.Yoyo).OnComplete(() => {
+                StartCoroutine(BlinkStar(stars[index]))
+            });
+
+            yield return null;
         }
 
         public void Select()
