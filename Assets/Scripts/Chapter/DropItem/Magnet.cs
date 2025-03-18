4f6fa81487019bae82abc8b88a69519d5ae4f2b9
ZUN <94329618+HSZ2809@users.noreply.github.com>
2025-03-18 10:50:15 +0900

    Update Magnet.cs
    자석 로직 구현

--- Assets/Scripts/Chapter/DropItem/Magnet.cs
+++ Assets/Scripts/Chapter/DropItem/Magnet.cs
@@ -1,16 +1,47 @@
+using System.Collections;
 using UnityEngine;
 
-public class Magnet : MonoBehaviour
+public class Magnet : MonoBehaviour, IGetDropedItem
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+        [SerializeField] private Collider2D col;
+        [SerializeField] private float initialSpeed;
+        float gravity = 50f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        public void GetDropedItem(Character character)
+        {
+            StartCoroutine(MoveTowardCharacter(character));
+        }
+
+        IEnumerator MoveTowardCharacter(Character character)
+        {
+            col.enabled = false;
+            float speed = initialSpeed;
+
+            Transform target = character.transform;
+            Vector3 direction = (transform.position - target.position).normalized;
+
+            while(speed > 0f)
+            {
+                transform.position += speed * Time.deltaTime * direction;
+                speed -= gravity * Time.deltaTime;
+                yield return null;
+            }
+
+            while (Vector3.Distance(transform.position, target.position) > 0.3f)
+            {
+                speed += gravity * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+                yield return null;
+            }
+
+            transform.position = target.position;
+
+            // 자석 취득 시 사운드 + 시각 효과 추가 필요
+            GameObject[] dropItems = GameObject.FindGameObjectsWithTag("DropedItem");
+            foreach (GameObject dropItem in dropItems)
+                if (TryGetComponent<EXPShard>(out var shard))
+                    shard.GetDropedItem(character);
+            
+            Destroy(this);
+        }
 }
