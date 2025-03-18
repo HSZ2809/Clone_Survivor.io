9e55e00177a11fcc282074546569e88808c0a3f2
ZUN <94329618+HSZ2809@users.noreply.github.com>
2025-03-18 10:29:40 +0900

    Update Bomb.cs
    필드 드랍 아이템 폭탄 로직 구현

--- Assets/Scripts/Chapter/DropItem/Bomb.cs
+++ Assets/Scripts/Chapter/DropItem/Bomb.cs
@@ -1,16 +1,51 @@
+using System.Collections;
 using UnityEngine;
 
-public class Bomb : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+namespace ZUN
+{    
+    public class Bomb : MonoBehaviour, IGetDropedItem
     {
-        
-    }
+        [SerializeField] private Collider2D col;
+        [SerializeField] private float attackPoint;
+        [SerializeField] private float initialSpeed;
+        float gravity = 50f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void GetDropedItem(Character character)
+        {
+            StartCoroutine(MoveTowardCharacter(character));
+        }
+
+        IEnumerator MoveTowardCharacter(Character character)
+        {
+            col.enabled = false;
+            float speed = initialSpeed;
+
+            Transform target = character.transform;
+            Vector3 direction = (transform.position - target.position).normalized;
+
+            while(speed > 0f)
+            {
+                transform.position += speed * Time.deltaTime * direction;
+                speed -= gravity * Time.deltaTime;
+                yield return null;
+            }
+
+            while (Vector3.Distance(transform.position, target.position) > 0.3f)
+            {
+                speed += gravity * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+                yield return null;
+            }
+
+            transform.position = target.position;
+
+            // 화면 전체가 0.3f 에 걸쳐서 하얗게 되었다가 다시 돌아오는 로직 필요
+            GameObject[] monsters = GameObject.FindGameObjectsWithTag("Monster");
+            foreach (GameObject monster in monsters)
+                if (TryGetComponent<IMon_Damageable>(out var damageable))
+                    damageable.TakeDamage(attackPoint);
+            
+            Destroy(this);
+        }
     }
 }
