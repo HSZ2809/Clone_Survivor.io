7461f090ca8d5c2901deba565402c52c4497ac22
ZUN <94329618+HSZ2809@users.noreply.github.com>
2025-03-18 13:36:52 +0900

    Update AudioSettings.cs
    오디오 세팅 로직 변경

--- Assets/Scripts/AudioSettings.cs
+++ Assets/Scripts/AudioSettings.cs
@@ -6,26 +6,20 @@ namespace ZUN
     [CreateAssetMenu(fileName = "AudioSettings", menuName = "Scriptable Objects/AudioSettings")]
     public class AudioSettings : ScriptableObject
     {
-        private float musicVolume;
-        private float effectVolume;
+        private bool isMusicMute;
+        private bool isEffeckSoundMute;
 
-        public float MusicVolume { get { return musicVolume; } }
-        public float EffectVolume { get { return effectVolume; } }
+        public float MusicVolume { get { (isMusicMute) ? 0.0f : 1.0f; } }
+        public float EffectVolume { get { (isEffectSoundMute) ? 0.0f : 1.0f; } }
 
         public void OnOffMusic()
         {
-            if (musicVolume >= 1.0f)
-                musicVolume = 0.0f;
-            else
-                musicVolume = 1.0f;
+            isMusicMute = !isMusicMute;
         }
 
         public void OnOffEffectSound()
         {
-            if (effectVolume >= 1.0f)
-                effectVolume = 0.0f;
-            else
-                effectVolume = 1.0f;
+            isEffeckSoundMute = !isEffeckSoundMute;
         }
     }
 }
