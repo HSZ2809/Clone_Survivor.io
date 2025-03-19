3d5f0592cb28feddb34d0ef5ca20b3e789fc1582
ZUN <94329618+HSZ2809@users.noreply.github.com>
2025-03-19 17:06:01 +0900

    Update PassiveSkill.cs

--- Assets/Scripts/Skill/PassiveSkill.cs
+++ Assets/Scripts/Skill/PassiveSkill.cs
@@ -5,5 +5,11 @@ namespace ZUN
     public abstract class PassiveSkill : Skill
     {
         // ScriptableObject 로 만들어진 ActiveSkillInfo 의 배열 필요
+
+        private void Awake()
+         {
+             character = GameObject.FindGameObjectWithTag("Character").GetComponent<Character>();
+             level = 1;
+         }
     }
 }
