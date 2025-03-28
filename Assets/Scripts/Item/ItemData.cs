cc353ebcbeaef6615df3824d94f15f82d13bb1ab
ZUN <94329618+HSZ2809@users.noreply.github.com>
2025-03-28 09:55:20 +0900

    Update ItemData.cs
    1. 네임스페이스로 레핑
    2. 속성 추가

--- Assets/Scripts/Item/ItemData.cs
+++ Assets/Scripts/Item/ItemData.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ItemData", menuName = "Scriptable Objects/ItemData")]
-public class ItemData : ScriptableObject
-{
-    
+namespace ZUN
+{    
+    [CreateAssetMenu(fileName = "ItemData", menuName = "Scriptable Objects/ItemData")]
+    public class ItemData : ScriptableObject
+    {
+        [SerializeField] int    id;
+        [SerializeField] string itemName;
+        [SerializeField] string tooltip;
+        [SerializeField] Sprite iconSprite;
+
+        public int    Id         { get { return id; } }
+        public string ItemName   { get { return itemName; } }
+        public string Tooltip    { get { return tooltip; } }
+        public Sprite IconSprite { get { return iconSprite; } }
+    }
 }
