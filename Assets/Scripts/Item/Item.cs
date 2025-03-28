37b165cb4a41050c7281ad28e1f614d3c15de5f2
ZUN <94329618+HSZ2809@users.noreply.github.com>
2025-03-28 11:08:04 +0900

    Update Item.cs
    1. 네임스페이스 레핑
    2. 속성, 생성자 추가

--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
 
-public class Item
+namespace ZUN
 {
-    
+    public class Item
+    {
+        public ItemData Data   { get; private set; }
+        public int      amount { get; private set; }
+        
+        public Item(ItemData data) => Data = data;
+    }
 }
