92d2ec1fdd36b22e07aa86d8c312c51e19f6294a
ZUN <94329618+HSZ2809@users.noreply.github.com>
2025-03-28 17:32:03 +0900

    Update SkillItemData.cs
    속성, 프로퍼티 추가

--- Assets/Scripts/Item/SkillItemData.cs
+++ Assets/Scripts/Item/SkillItemData.cs
@@ -1,7 +1,32 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "SkillItemData", menuName = "Scriptable Objects/SkillItemData")]
-public class SkillItemData : ScriptableObject
+namespace ZUN
 {
-    
+    [CreateAssetMenu(fileName = "SkillItemData", menuName = "Scriptable Objects/SkillItemData")]
+    public class SkillItemData : ScriptableObject
+    {
+        [SerializeField] int           id;
+        [SerializeField] string        itemName;
+        [SerializeField] string        comment;
+        [SerializeField] Sprite        iconSprite;
+        //[SerializeField] int           maxLevel;
+        [SerializeField] EquipmentType type;
+
+        public int           Id         => id;
+        public string        ItemName   => itemName;
+        public string        Comment    => comment;
+        public Sprite        IconSprite => iconSprite;
+        //public int           MaxLevel   => maxLevel;
+        public EquipmentType Type       => type;
+
+        public enum EquipmentType
+        {
+            Weapom;
+            Cloth;
+            Necklace;
+            Belt;
+            Gloves;
+            Shoes;
+        }
+    }
 }
