893b94d3e7c60f5059b7cc37912d2f31199d0e28
홍성준 <6255plm@gmail.com>
2025-04-21 22:42:22 +0900

    동기화
    1. 작업 공간 동기화

--- Assets/UnityTechnologies/CustomPlayables/MonsterSpawn/MonsterSpawnTrack.cs
+++ Assets/UnityTechnologies/CustomPlayables/MonsterSpawn/MonsterSpawnTrack.cs
@@ -5,13 +5,13 @@ namespace ZUN
 {
     [TrackColor(1f, 0.4562354f, 0f)]
     [TrackClipType(typeof(MonsterSpawnClip))]
-    [TrackBindingType(typeof(MonsterSpawn_Circle))]
+    [TrackBindingType(typeof(MonsterSpawn))]
     public class MonsterSpawnTrack : TrackAsset
     {
         public override void GatherProperties(PlayableDirector director, IPropertyCollector driver)
         {
 #if UNITY_EDITOR
-            MonsterSpawn_Circle trackBinding = director.GetGenericBinding(this) as MonsterSpawn_Circle;
+            MonsterSpawn trackBinding = director.GetGenericBinding(this) as MonsterSpawn;
             if (trackBinding == null)
                 return;
 #endif
